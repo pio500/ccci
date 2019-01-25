89feba1994ec0a2ac87fa7cd2970d8a2a8989f2a
Eojin <pio500@naver.com>
2019-01-25 11:11:08 +0900

    docker-compose-server midi

--- src/provisioning.sh
+++ src/provisioning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+sleep 10
 ## Define the Variables
 #Target_name="tutorial"
 #User="test"
@@ -10,20 +10,16 @@
 Target_name=$CONCOURSE_TARGET_NAME
 User=$CONCOURSE_USER
 Password=$CONCOURSE_PASSWORD
-Concourse_server=$CONCOURSE_SERVER
+Concourse_server=$CONCOURSE_WEB_SERVER
 
-Pipeline_basic="./pipeline.yml"
+#Pipeline_basic="./pipeline.yml"
 Pipeline_name="scratch"
 
-
-
-
-sleep 5
 ## Create a new Target via Admin(Default Admin: test/test)
-fly -t $Target_name login -c $Concourse_server -n main --username=$User --password=$Password
+##fly -t $Target_name login -c $Concourse_server -n main --username=$User --password=$Password
 
 ## Show Target list
-fly targets
+##fly targets
 
 
 ## Purge
@@ -31,11 +27,11 @@ fly targets
 #fly -t $Target_name destroy-pipeline -p canary
 
 ## Setup Default sample pipeline
-fly -t $Target_name set-pipeline -c $Pipeline_basic -p $Pipeline_name -n
+#fly -t $Target_name set-pipeline -c $Pipeline_basic -p $Pipeline_name -n
 
 ## Create a new pipeline
-cd /pipelines
-fly -t $CONCOURSE_TARGET_NAME set-pipeline -p $CONCOURSE_DEFAULT_PIPELINE -c pipeline.yml -l credentials.yml  --non-interactive
+#cd /pipelines
+#fly -t $CONCOURSE_TARGET_NAME set-pipeline -p $CONCOURSE_DEFAULT_PIPELINE -c pipeline.yml -l credentials.yml  --non-interactive
 
 ## You can Unpause directly before the UI connect
 #fly -t $CONCOURSE_TARGET_NAME unpause-pipeline -p $CONCOURSE_DEFAULT_PIPELINE
@@ -43,5 +39,17 @@ fly -t $CONCOURSE_TARGET_NAME set-pipeline -p $CONCOURSE_DEFAULT_PIPELINE -c pip
 #fly -t $Target_name set-pipeline -p bluegreen -c ./ci/pipeline-bluegreen.yml -l ../ci-credentials-online.yml  --non-interactive
 #fly -t $Target_name set-pipeline -p canary -c ./ci/pipeline-canary.yml -l ../ci-credentials-online.yml  --non-interactive
 
-tail -f /var/log/lastlog
+#tail -f /var/log/lastlog
 
+
+## Please make sure the variable as below
+#declare -x AIRPLAINE_PORT
+#declare -x AIRPLAINE_USER
+#declare -x AIRPLAINE_PASSWORD
+#declare -x CONCOURSE_WEB_SERVER
+
+## Get airplane
+
+echo $CONCOURSE_WEB_SERVER
+## Please make sure the Env. variable such as this config file ./docker-compose-server.yml to below
+/usr/bin/airplane --password="$AIRPLAINE_PASSWORD" --user="$AIRPLAINE_USER" --concourse_server="$CONCOURSE_WEB_SERVER" --port="$AIRPLAINE_PORT"
